e4cf3a3ccfddcfc6741deba0640cf1407e0b8e02
MyYeonbi <yeonbi_kim@icloud.com>
2024-07-09 22:21:40 +0900

    Initial commit

---
+++ untitled/src/scanner_quiz/Q2_price.java
@@ -0,0 +1,23 @@
+package scanner_quiz;
+import java.util.Scanner;
+public class Q2_price {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("상품의 가격을 입력하세요(-1을 입력하면 종료): ");
+            int price = sc.nextInt();
+            if (price == -1){
+                System.out.println("프로그램을 종료합니다.");
+                break;}
+                System.out.print("상품의 수량을 입력하세요 : ");
+                int quantity = sc.nextInt();
+                sc.nextLine();
+
+                System.out.println("총 비용"+ price*quantity);
+
+
+        }
+    }
+}
